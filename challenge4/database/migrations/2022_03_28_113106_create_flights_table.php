b735cb98a2bf46e79d63b4991c1f59b2be32912a
ppalou2000 <ppalou2000@icloud.com>
2022-03-28 10:43:31 -0300

    create migrations and models

---
+++ challenge4/database/migrations/2022_03_28_113106_create_flights_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('origin_id');
+            $table->foreignId('origin_id')->references('id')->on('cities');
+            
+            $table->unsignedBigInteger('destination_id');
+            $table->foreignId('destination_id')->references('id')->on('cities');
+            
+            $table->time('takeOff');
+            $table->time('landing');
+            
+            $table->unsignedBigInteger('airline_id');
+            $table->foreignId('airline_id')->references('id')->on('airlines');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flights');
+    }
+};
